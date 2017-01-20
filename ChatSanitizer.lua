d15f28e449289ad96a0286f6685df74564c4d6db
Aviana <winblocker@gmx.de>
2017-01-20 10:49:34 +0100

    Fix empty chatframe

--- ChatSanitizer.lua
+++ ChatSanitizer.lua
@@ -4,17 +4,19 @@ local function filter(text)
 	local count = strlen(gsub(text,"([%w%s%p]+)",""))
 	if count < 3 then
 		return text
+	else
+		return ""
 	end
 end
 
-local ChatSanitizer = CreateFrame("Frame")
+local ChatSanitizer = CreateFrame("Frame", "ChatSanitizer")
 ChatSanitizer:RegisterEvent("ADDON_LOADED")
 
 local function OnEvent()
 	if arg1 == "FuBar_WhisperFu" then
 		function WhisperFu:OnReceiveWhisper()
 			arg1 = filter(arg1)
-			if arg1 then
+			if arg1 ~= "" then
 				self:ProcessWhisper(false)
 			end
 		end
@@ -22,7 +24,7 @@ local function OnEvent()
 		Old_WIM_FilterResult = WIM_FilterResult
 		WIM_FilterResult = function(msg)
 			local filtered = filter(msg)
-			if filtered then
+			if filtered ~= "" then
 				return Old_WIM_FilterResult(msg)
 			else
 				return 1
@@ -34,11 +36,11 @@ end
 ChatSanitizer:SetScript("OnEvent", OnEvent)
 
 local orig = ChatFrame_OnEvent
-ChatFrame_OnEvent = function(event)
+ChatFrame_OnEvent = function()
 	if event == "CHAT_MSG_CHANNEL" or event == "CHAT_MSG_WHISPER" or event == "CHAT_MSG_SAY" or event == "CHAT_MSG_YELL" or event == "CHAT_MSG_BATTLEGROUND" or event == "CHAT_MSG_BATTLEGROUND_LEADER" or event == "CHAT_MSG_EMOTE" then
 		arg1 = filter(arg1)
 	end
-	if arg1 then
+	if arg1 ~= "" then
 		return orig(event)
 	end
 end
